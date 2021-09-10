25fa253f84f1b089eadd99422160ceaa5a2b7c9e
risto <risto.kowaczewski@gmail.com>
2021-09-11 00:16:31 +0200

    plugin test

---
+++ tracardi_tests/plugins/test_event_scheduler.py
@@ -0,0 +1,40 @@
+import time
+
+from tracardi.domain.entity import Entity
+from tracardi.domain.event import Event
+from tracardi.domain.profile import Profile
+from tracardi.domain.session import Session
+from tracardi_plugin_sdk.service.plugin_runner import run_plugin
+
+from tracardi_event_scheduler.plugin import EventSchedulerAction
+
+
+def test_event_scheduler():
+    init = {
+        "event_type": "new-event",
+        "postpone": "+2m"
+    }
+    payload = {}
+
+    profile = Profile(id="1")
+    event = Event(
+        id='1',
+        type='text',
+        session=Session(id='1'),
+        context={},
+        source=Entity(id='d94ffd0f-ee16-40f9-a81b-61893f20aa1f')
+    )
+    session = Session(
+        id='1'
+    )
+
+    result = run_plugin(
+        EventSchedulerAction,
+        init,
+        payload,
+        profile,
+        session,
+        event
+)
+    assert result.output.port == 'payload'
+    assert result.output.value['timestamp'] > time.time()
