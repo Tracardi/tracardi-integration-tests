7288f38ed66c9cb24cfa4d3e5a9bace32c1a57d3
risto <risto.kowaczewski@gmail.com>
2021-10-20 11:13:19 +0200

    new tests.

--- tracardi_tests/end_2_end/test_overriding_profile_traits.py
+++ tracardi_tests/end_2_end/test_overriding_profile_traits.py
@@ -1,18 +1,12 @@
 from uuid import uuid4
-
 from tracardi.process_engine.action.v1.increase_views_action import IncreaseViewsAction
-
 from tracardi.domain.flow import Flow
-
 from tracardi.process_engine.action.v1.end_action import EndAction
 from tracardi.process_engine.action.v1.increase_visits_action import IncreaseVisitsAction
-
 from tracardi.process_engine.action.v1.start_action import StartAction
-
 from tracardi.process_engine.action.v1.debug_payload_action import DebugPayloadAction
 from tracardi.process_engine.action.v1.traits.append_trait_action import AppendTraitAction
 from tracardi.process_engine.action.v1.traits.copy_trait_action import CopyTraitAction
-
 from tracardi_tests.api.test_resource import create_resource
 from tracardi_graph_runner.service.builders import action
 from tracardi_tests.utils.utils import Endpoint
