e373f3c105964d0289b8673a5bcf887f0de40f6b
risto <risto.kowaczewski@gmail.com>
2021-10-21 02:44:03 +0200

    new tests.

--- tracardi_tests/plugins/unit/test_regex_plugin.py
+++ tracardi_tests/plugins/unit/test_regex_plugin.py
@@ -4,8 +4,10 @@ from tracardi_regex_validator.plugin import RegexValidatorAction
 
 def test_regex_validator_plugin():
     payload = {'data': "12:32"}
-    init = {'validation_regex': r'^([0-1]?[0-9]|2[0-3]):[0-5][0-9](:?)([0-5]?[0-9]?)$',
-            'data': "payload@data"}
+    init = {
+        'validation_regex': r'^([0-1]?[0-9]|2[0-3]):[0-5][0-9](:?)([0-5]?[0-9]?)$',
+        'data': "payload@data"
+    }
 
     result = run_plugin(RegexValidatorAction, init, payload)
 
