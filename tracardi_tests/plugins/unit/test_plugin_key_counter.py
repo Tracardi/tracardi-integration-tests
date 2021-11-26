c432297043c9a2ad34fce3c8377c815768adf41e
risto <risto.kowaczewski@gmail.com>
2021-11-26 16:03:09 +0100

    upgraded tests.

--- tracardi_tests/plugins/unit/test_plugin_key_counter.py
+++ tracardi_tests/plugins/unit/test_plugin_key_counter.py
@@ -11,10 +11,11 @@ def test_key_counter():
     c.count('a')
     c.count('b')
     c.count(['a', 'c'])
+    c.count({"a": 10, "b": 1, "f": 1, "g": "asas"})
 
     result = c.counts
 
-    assert result == {'d': 1, 'a': 2, 'b': 1, 'c': 1}
+    assert result == {'d': 1, 'a': 12, 'b': 2, 'c': 1, 'f': 1}
 
 
 def test_key_counter_plugin():
